0e6c1492a8e0a285140863ff0fae56101df05213
Alexandre <a.scanzalupi@gmail.com>
2022-11-30 11:13:31 +0100

    Premiers pas pour la classe principale

---
+++ Personnage.cpp
@@ -0,0 +1,6 @@
+#include <Personnage.hpp>
+
+//Personnage::Personnage (int Force, int dexterite, int constitution, int intelligence, int perception, int charisme)
+//{}
+
+
---
+++ Personnage.hpp
@@ -0,0 +1,14 @@
+#ifndef __PERSONNAGE_HPP
+#define __PERSONNAGE_HPP
+
+class Personnage
+{
+	int Force;
+	int Dexterite;
+	int Constitution;
+	int Intelligence;
+	int Perception;
+	int Charisme;
+};
+
+#endif //__PERSONNAGE_HPP
